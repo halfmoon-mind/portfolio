1172e6af39ab561b3a5bbf501b1562203ab6193d
halfmoon-mind <5338095@gmail.com>
2025-04-18 01:02:55 +0900

    feat: 배포 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://halfmoon-mind.netlify.app/",
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
