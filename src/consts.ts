a760c21e3bdc329ac9e764ab89f0196b4e01f9ac
halfmoon-mind <5338095@gmail.com>
2025-04-18 10:03:40 +0900

    feat: 사이트 제목 및 설명 업데이트

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Half to Full";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Half to Full';
+export const SITE_DESCRIPTION = '최고의 경험을 위해 노력하는 일기를 작성하는 공간';
