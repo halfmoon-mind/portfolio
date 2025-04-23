767aceb6a415c32545d9804a408fb39d91bc8df3
halfmoon-mind <5338095@gmail.com>
2025-04-23 15:31:21 +0900

    feat: 사이트 URL 변경 및 .gitignore에 .netlify/build/ 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://halfmoon-mind.netlify.app/",
+  site: 'https://halfmoon.day/',
   integrations: [mdx(), sitemap()],
   adapter: netlify(),
 });
