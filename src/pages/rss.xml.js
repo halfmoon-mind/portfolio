a30141610ab0c5780b53e4f70cef759af93f2e66
halfmoon-mind <5338095@gmail.com>
2026-04-21 19:54:31 +0900

    feat(clips): include clips in RSS feed merged with blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,28 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const blogPosts = await getCollection('blog');
+	const clips = await getCollection('clips');
+
+	const items = [
+		...blogPosts.map((post) => ({
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.pubDate,
+			link: `/blog/${post.id}/`,
+		})),
+		...clips.map((clip) => ({
+			title: clip.data.title,
+			description: clip.data.description,
+			pubDate: clip.data.pubDate,
+			link: `/clips/${clip.id}/`,
+		})),
+	].sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
+		items,
 	});
 }
